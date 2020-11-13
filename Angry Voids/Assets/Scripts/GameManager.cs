8b48091aa34575abb85edf397461d05a7cb95237
MustacheLincoln <MustacheLincoln@gmail.com>
2020-11-13 16:29:09 -0600

    Added Remaining Text

--- Angry Voids/Assets/Scripts/GameManager.cs
+++ Angry Voids/Assets/Scripts/GameManager.cs
@@ -1,11 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
+    [SerializeField] Text _remainingText;
+
+    Object[] _objects;
+
+    void OnEnable()
     {
-        Time.timeScale = 1;
+        _objects = FindObjectsOfType<Object>();
+    }
+
+    void Update()
+    {
+        if (ObjectsAreAllGone())
+            GoToNextLevel();
+        _remainingText.text = "Remaining: " + _objects.Length;
+    }
+
+    private bool ObjectsAreAllGone()
+    {
+        _objects = FindObjectsOfType<Object>();
+        if (_objects.Length > 0)
+            return false;
+        return true;
+    }
+
+    private void GoToNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
